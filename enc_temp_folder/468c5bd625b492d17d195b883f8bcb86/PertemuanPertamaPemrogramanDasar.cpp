935a57d6462b95556b963d51396dff42a6300675
ardiapri <ardiapri882@gmail.com>
2024-02-27 19:18:55 +0700

    Add project files.

---
+++ enc_temp_folder/468c5bd625b492d17d195b883f8bcb86/PertemuanPertamaPemrogramanDasar.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+
+    cout << "Masukkan jari-jari = ";
+    cin >> a;
+
+    b = 3.14 * a * a;
+
+    cout << "Luas Lingkaran Adalah = " << b << endl;
+
+    system("pause");
+
+}
+
+
+
